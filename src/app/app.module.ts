c477c314dc76cc2bd5acdc2e969a2c38e4067bae
tanch1eu <dchieu.nt@gmail.com>
2022-10-24 10:52:29 +1000

    On branch master Changes to be committed: 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/calendar/calendar.component.html 	new file:   src/app/calendar/calendar.component.scss 	new file:   src/app/calendar/calendar.component.spec.ts 	new file:   src/app/calendar/calendar.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
+import { CalendarComponent } from './calendar/calendar.component';
+import { NbCalendarModule } from '@nebular/theme';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CalendarComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule,
+    NbCalendarModule,
+    NbCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
